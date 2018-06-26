af0a5f7e207b80b6b7c8850c4cc8b3c0fba90eaf
joshua <josh43k@gmail.com>
2018-06-25 18:00:18 -0700

    #

--- js/scripts.js
+++ js/scripts.js
@@ -5,107 +5,129 @@ let toggleClass = document.querySelector('#show_done');
 let header = document.querySelector('#header');
 let todolist = [];
 let doneList = [];
-let i = 0;
+let listValue = 0;
 
-function Todoitems(div, text, removeBtn, doneBtn, editBtn, removeSpan, doneSpan, editSpan) {
-  this.div = div;
+function Todoitems(text) {
   this.text = text;
-  this.removeBtn = removeBtn;
-  this.doneBtn = doneBtn;
-  this.editBtn = editBtn;
-  this.removeSpan = removeSpan;
-  this.doneSpan = doneSpan;
-  this.editSpan = editSpan;
+}
+
+function getSaved() {
+
+};
+
+function setSaved() {
+  localStorage.setItem('todoList', JSON.stringify(todolist));
+  localStorage.setItem('doneList', JSON.stringify(doneList));
 }
 
 submitBtn.addEventListener('click', e => {
   e.preventDefault();
   let userNote = document.forms.noteForm.noteInput.value;
-  let newTodo = new Todoitems(document.createElement('div'), document.createElement('h2'), document.createElement('button'), document.createElement('button'), document.createElement('button'), document.createElement('span'), document.createElement('span'), document.createElement('span'));
-  createTodo(userNote,newTodo);
+  let newTodo = new Todoitems(userNote);
+  createTodo(todoContainer,userNote);
   document.forms.noteForm.reset();
   todolist.push(newTodo);
+  setSaved();
 });
 
 toggleClass.addEventListener('click', e => {
-  switch (i) {
+  switch (listValue) {
     case 0:
     header.textContent = 'Done list';
     todoContainer.classList.add('hide');
     doneContainer.classList.remove('hide');
-    i++
+    listValue++;
       break;
     case 1:
     header.textContent = 'Todo list';
     doneContainer.classList.add('hide');
     todoContainer.classList.remove('hide');
-    i--;
+    listValue--;
       break;
     default:
-    i = 0
+    listValue = 0;
   }
 });
 
 
 
-function createTodo(todo,items) {
-  let div = items.div,
-    text = items.text,
-    removeBtn = items.removeBtn,
-    doneBtn = items.doneBtn,
-    editBtn = items.editBtn,
-    removeSpan = items.removeSpan,
-    doneSpan = items.doneSpan,
-    editSpan = items.editSpan;
-  todoContainer.appendChild(div);
-  div.appendChild(text);
+function createTodo(con,todo) {
+  let container = con,
+    text = todo,
+    div = document.createElement('div'),
+    h2 = document.createElement('h2'),
+    removeBtn = document.createElement('button'),
+    okBtn = document.createElement('button'),
+    editBtn = document.createElement('button');
+  container.appendChild(div);
   div.classList.add('listed-item');
-  text.textContent = todo;
+  div.appendChild(h2);
+  h2.textContent = text;
   div.appendChild(removeBtn);
-  removeBtn.appendChild(removeSpan);
-  removeSpan.classList.add('glyphicon','glyphicon-remove');
+  removeBtn.classList.add('glyphicon','glyphicon-remove');
   removeBtn.addEventListener('click', e => {
-    todoContainer.removeChild(div);
+    e.preventDefault();
+    container.removeChild(div);
+    if(text === text)
+    console.log(todolist);
+    setSaved();
   });
-  div.appendChild(doneBtn);
-  doneBtn.appendChild(doneSpan);
-  doneSpan.classList.add('glyphicon','glyphicon-ok');
-  doneBtn.addEventListener('click', e => {
-    todoContainer.removeChild(div);
-    swapDone(todo,items);
+  div.appendChild(okBtn);
+  okBtn.classList.add('glyphicon','glyphicon-ok');
+  okBtn.addEventListener('click', e => {
+    e.preventDefault();
+    container.removeChild(div);
+    if(container === todoContainer){
+      createTodo(doneContainer,text);
+    } else if(container === doneContainer){
+      createTodo(todoContainer,text);
+    };
   });
   div.appendChild(editBtn);
-  editBtn.appendChild(editSpan);
-  editSpan.classList.add('glyphicon','glyphicon-pencil');
-};
+  editBtn.classList.add('glyphicon','glyphicon-edit');
+  editBtn.addEventListener('click', e => {
+    e.preventDefault();
+    editFun(div,h2,removeBtn,okBtn,editBtn);
+});
 
-function swapDone(todo,items) {
-  let div = items.div,
-    text = items.text,
-    removeBtn = items.removeBtn,
-    doneBtn = items.doneBtn,
-    editBtn = items.editBtn,
-    removeSpan = items.removeSpan,
-    doneSpan = items.doneSpan,
-    editSpan = items.editSpan;
-  doneContainer.appendChild(div);
-  div.appendChild(text);
-  div.classList.add('listed-item');
-  text.textContent = todo;
-  div.appendChild(removeBtn);
-  removeBtn.appendChild(removeSpan);
-  removeSpan.classList.add('glyphicon','glyphicon-remove');
-  removeBtn.addEventListener('click', e => {
-    todoContainer.removeChild(div);
+
+function editFun(div,h2,btn1,btn2,btn3) {
+  h2.classList.add('hide');
+  btn1.classList.add('hide');
+  btn2.classList.add('hide');
+  btn3.classList.add('hide');
+  let form = document.createElement('form'),
+    input = document.createElement('input'),
+    saveBtn = document.createElement('button'),
+    stopBtn = document.createElement('button');
+  div.appendChild(form);
+  form.classList.add('note-form');
+  form.setAttribute('name','newForm');
+  form.appendChild(input);
+  input.classList.add('note-input');
+  input.setAttribute('placeholder','New note');
+  input.setAttribute('name','newNote');
+  form.appendChild(saveBtn);
+  saveBtn.classList.add('glyphicon','glyphicon-floppy-saved');
+  saveBtn.setAttribute('name','submit');
+  saveBtn.addEventListener('click', e => {
+    e.preventDefault();
+    let saveInput = input.value;
+    h2.textContent = saveInput;
+    div.removeChild(form);
+    h2.classList.remove('hide');
+    btn1.classList.remove('hide');
+    btn2.classList.remove('hide');
+    btn3.classList.remove('hide');
   });
-  div.appendChild(doneBtn);
-  doneBtn.appendChild(doneSpan);
-  doneSpan.classList.add('glyphicon','glyphicon-play');
-  doneBtn.addEventListener('click', e => {
-    doneContainer.removeChild(div);
-    createTodo(todo,items);
+  form.appendChild(stopBtn);
+  stopBtn.classList.add('glyphicon','glyphicon-floppy-remove');
+  stopBtn.addEventListener('click', e => {
+    div.removeChild(form);
+    h2.classList.remove('hide');
+    btn1.classList.remove('hide');
+    btn2.classList.remove('hide');
+    btn3.classList.remove('hide');
   });
-  div.appendChild(editBtn);
-  editBtn.appendChild(editSpan);
-  editSpan.classList.add('glyphicon','glyphicon-edit');
+};
 }
